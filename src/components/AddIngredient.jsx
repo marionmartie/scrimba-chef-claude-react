3b5016fb18cad2318751ff139537f27bb75d27e2
Marion Balba <balba.marion@gmail.com>
2025-01-07 10:15:04 +0800

    Used state for ingredients

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 
 const AddIngredient = () => {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+
+    const addIngredient = (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get('ingredient')
+        setIngredients( (prevIngredients) => [...prevIngredients, newIngredient] )
+    }
 
     return (
         <section className="w-screen mx-auto bg-off-white pt-11 pb-6">
-            <div className="flex justify-center gap-4">
-                <input className="rounded p-2 w-30 border-2" type="text" placeholder="eg. raddish" id="btnAddIngredient" aria-label="add-ingredient" />
-                <button className="rounded bg-gray-900 text-white px-5">+ Add Ingredient</button>
+            <form action="" onSubmit={addIngredient}>
+                <div className="flex justify-center gap-4">
+                    <input name="ingredient" className="rounded p-2 w-30 border-2" type="text" placeholder="eg. raddish" id="btnAddIngredient" aria-label="add-ingredient" />
+                    <button className="rounded bg-gray-900 text-white px-5">+ Add Ingredient</button>
+                </div>
+            </form>
+            <div className="flex justify-center mt-4">
+                <ul className="list-disc">
+                    {
+                        ingredients.map(el => <li> {el} </li>)
+                    }
+                </ul>
             </div>
-            <ul>
-            {
-                ingredients.map(el =>  <li> { el } </li> )
-            }
-            </ul>
         </section>
     )
 }
