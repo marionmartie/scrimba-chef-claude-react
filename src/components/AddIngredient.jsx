7bc67fa0adbc87ed4327c00a6b2e8d6b3224cc72
Marion Balba <balba.marion@gmail.com>
2025-01-08 08:50:07 +0800

    Upgraded to react 19 and uses form action

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -3,16 +3,14 @@ import { useState } from "react"
 const AddIngredient = () => {
     const [ingredients, setIngredients] = useState([])
 
-    const addIngredient = (event) => {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    const addIngredient = (formData) => {
         const newIngredient = formData.get('ingredient')
         setIngredients( (prevIngredients) => [...prevIngredients, newIngredient] )
     }
 
     return (
         <section className="w-screen mx-auto bg-off-white pt-11 pb-6">
-            <form action="" onSubmit={addIngredient}>
+            <form action={addIngredient}>
                 <div className="flex justify-center gap-4">
                     <input name="ingredient" className="rounded p-2 w-30 border-2" type="text" placeholder="eg. raddish" id="btnAddIngredient" aria-label="add-ingredient" />
                     <button className="rounded bg-gray-900 text-white px-5">+ Add Ingredient</button>
@@ -21,7 +19,7 @@ const AddIngredient = () => {
             <div className="flex justify-center mt-4">
                 <ul className="list-disc">
                     {
-                        ingredients.map(el => <li> {el} </li>)
+                        ingredients.map(el => <li key={el}> {el} </li>)
                     }
                 </ul>
             </div>
